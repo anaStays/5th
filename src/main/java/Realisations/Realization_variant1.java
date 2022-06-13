0b6a99a79c147b4a76dd719871a25d9f4a226241
an_vo <an_vorotilina@mail.ru>
2022-06-14 02:36:49 +0300

    fix implementation issues

--- src/main/java/Realisations/Realization_variant1.java
+++ src/main/java/Realisations/Realization_variant1.java
@@ -3,6 +3,6 @@ package Realisations;
 import Interfaces.Inject_variant1;
 
 
-public class Realization_variant1 {
+public class Realization_variant1 implements Inject_variant1{
     public void test1(){System.out.println("realization_variant1");}
 }
--- src/main/java/Realisations/Realization_variant2.java
+++ src/main/java/Realisations/Realization_variant2.java
@@ -2,6 +2,6 @@ package Realisations;
 
 import Interfaces.Inject_variant2;
 
-public class Realization_variant2 {
+public class Realization_variant2 implements Inject_variant2 {
     public void test2(){System.out.println("realization_variant2");}
 }
